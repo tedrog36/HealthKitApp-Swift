5eba865c2235d65ebd33cc8573bae7f310e8cc03
Ted Rogers <tedrog36@gmail.com>
2014-06-10 14:34:31 -0400

    Initial Commit

---
+++ HealthKitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKitApp
+//
+//  Created by Ted Rogers on 6/10/14.
+//  Copyright (c) 2014 Ted Rogers Consulting, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
